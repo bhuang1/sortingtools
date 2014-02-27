b8ebc8d14e7b931795b61095217a2ae6d0593732
caliChander <panoptic.biopower@gmail.com>
2014-02-26 20:21:13 -0500

    adding initial commit of insertionSort files

---
+++ js/insertionSort_functions.js
@@ -0,0 +1,72 @@
+/*
+ * Handles all selection sort specific functions.
+ * @author Betty Huang
+ * @author Sucharita Jayanti
+ * @author Matthew Joyce
+ * @author Mehdi Oulmakki
+ * @author Chandrasekar Ramesh
+ * @date February 13, 2014
+ * @version 0.1
+ * @since 0.1
+ */
+ 
+/*
+ * Check if a move is legal in manual mode.
+ * @param globals - object containing global variables
+ * @param ui      - JQuery ui element
+ * @return Whether the attempted position is legal
+ */
+function legalMove(globals, ui, start) {
+    return true;
+}
+
+/*
+ * Check if a move is legal in assisted mode.
+ * @param globals - object containing globals
+ * @param ui      - JQuery ui element
+ * @return Whether the attempted position is legal
+ */
+function assistedLegalMove(globals, ui, start) {
+    return false;
+}
+
+/*
+ * Checks if card is sorted or not so as to set the .sorted var
+ * @param globals - object containing global variables
+ * @param id      - id of the div element to check
+ */
+function isSorted(globals, id) {
+    for (var i = 0; i < globals.NUM_CARDS; i++) {
+        current = $('#0');
+        if (current.index() != i)
+            return false;
+    }
+    return true;
+}
+
+/*
+ * Starting at the card with index cardIndex, sets all following
+ * cards that happen to be sorted as sorted
+ * @param globals   - object containing global variables
+ * @param cardIndex - index of the card at which chainSort starts
+ */
+function chainSort(globals, cardIndex) {
+    // Propagates card flips
+    while (true) {
+        ++cardIndex;
+        if ($('#' + cardIndex).index() == cardIndex &&
+            globals.cards[cardIndex].flipped &&
+            $('#' + (cardIndex-1)).index() == cardIndex-1) {
+
+            globals.cards[cardIndex].sorted = true;
+            $('#' + cardIndex).css({
+                backgroundImage:'url('+globals.cards[cardIndex].sortedBack+')'
+            });
+        } else {
+            if (detectFinish(globals)) {
+                showFinish();
+            }
+            break;
+        }
+    }
+}
---
+++ js/insertionSort_skeleton.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    /* Globals that configure state space */
+    function Globals(){
+        this.SELECT_MOVE = 30,
+        this.SELECT_SPEED = 100,
+        this.WINDOW_WIDTH = $(window).width(),
+        this.NUM_CARDS = 13,
+        this.CARD_WIDTH = 120,
+        this.PADDING = 120,
+        this.SPACE = Math.floor((this.WINDOW_WIDTH - this.CARD_WIDTH - 2 * this.PADDING) / (this.NUM_CARDS - 1)),
+        this.MAX_FLIP = 16,
+        this.totFlip = 0,
+        this.ops = 0,
+        this.mem = 0,
+        this.maxCard = undefined
+    }
+
+    var globals = new Globals();
+
+    // Create the cards
+    createCards(globals);
+    createCardHTML(globals, '.sort-area');
+
+    // Hover over a card
+    handleHover(globals, '.card');
+
+    // Double click
+    handleDoubleClick(globals, '.card', isSorted, chainSort);
+
+    // Right click
+    handleRightClick(globals, isSorted, '.droppable', '.card', chainSort);
+
+    // Handle drag and drop
+    handleDragDrop(globals, '.sort-area', legalMove, isSorted, chainSort);
+	
+    // handle the popping
+    popHintsHTML();
+    popHints();
+    hideHints();
+});
